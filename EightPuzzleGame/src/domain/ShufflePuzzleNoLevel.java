a651bf708c27d208d55e8c9976102634f35a47e3
Makhles R. Lange <mr.lange85@gmail.com>
2017-04-12 14:59:06 -0300

    Added ShufflePuzzleNoLevel fake shuffler

---
+++ EightPuzzleGame/src/domain/ShufflePuzzleNoLevel.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class ShufflePuzzleNoLevel implements StrategyShufflePuzzle {
+
+    @Override
+    public void shuffle(Puzzle game) {
+        // Only make-believe.
+    }
+
+}
